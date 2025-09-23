628bec017dcddce651852e1c99b1da945a9f4e07
Vinícius da Cunha Ferreira <viniciusdacunha.ferreira@gmail.com>
2025-09-23 11:00:04 -0300

    Aula Criando Gerenciamento de Estado Único

---
+++ gerenciamento_estado/my_value_notifier/lib/main.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:my_value_notifier/my_change_notifier.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final controller = ThemeController();
+
+  void onChangeTheme() {
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    controller.addListener(onChangeTheme);
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.removeListener(onChangeTheme);
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: controller.value ? ThemeData.dark() : ThemeData.light(),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final controller = ThemeController();
+
+  void onChangeTheme() {
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    controller.addListener(onChangeTheme);
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.removeListener(onChangeTheme);
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: ListTile(
+          onTap: () => controller.changeTheme(),
+          title: Text('Tema escuro'),
+          trailing: Switch(
+            value: controller.value,
+            onChanged: (_) => controller.changeTheme(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ThemeController extends MyValueNotifier<bool> {
+  ThemeController._internal() : super(false);
+
+  static final ThemeController _instance = ThemeController._internal();
+
+  factory ThemeController() => _instance;
+
+  void changeTheme() {
+    value = !value;
+  }
+}
